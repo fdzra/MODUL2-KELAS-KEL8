5cb350e90d40ca01a84b475d2022b99c736484d2
Alfi Ahmad Fauzi <147068329+alpaai18@users.noreply.github.com>
2024-05-25 17:20:19 +0700

    Update web.php
    Fix Conflict Merging Web.php

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,18 @@
 <?php
-
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\PetugasController;
 use App\Http\Controllers\KasirController;
 use App\Http\Controllers\SesiController;
 use Illuminate\Support\Facades\Route;
 use \App\Http\Middleware\UserAkses;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\kelolaPelangganController;
 
 
 
 Route::middleware(['guest'])->group(function(){
     Route::get('/', [SesiController::class, 'index'])->name('login');
-    Route::post('/', [SesiController::class, 'login']);
+    Route::post('/login', [SesiController::class, 'login']);
     Route::get('/register', [SesiController::class, 'register']);
     Route::post('/create', [SesiController::class, 'create']);
 });
@@ -49,8 +50,23 @@ Route::middleware(['auth'])->group(function(){
 
     // INI BAGIAN ADMIN
     Route::get('/admin/admin', [AdminController::class, 'admin'])->middleware(UserAkses::class . ':admin');
+    Route::get('/admin/admin/kelolaPelanggan', [kelolaPelangganController::class, 'read'])->middleware(UserAkses::class . ':admin');
+
+    Route::get('/admin/admin/kelolaPelanggan/tambah_pelanggan', [kelolaPelangganController::class, 'create'])->middleware(UserAkses::class . ':admin');
+
+    Route::post('/admin/admin/kelolaPelanggan/pelanggan_store', [kelolaPelangganController::class, 'store'])->middleware(UserAkses::class . ':admin');
+
+    Route::get('/admin/admin/kelolaPelanggan/edit_pelanggan/{id}', [kelolaPelangganController::class, 'edit'])->middleware(UserAkses::class . ':admin');
+
+    Route::put('/admin/admin/kelolaPelanggan/{id}', [kelolaPelangganController::class, 'update'])->middleware(UserAkses::class . ':admin');
+
+    Route::delete('/admin/admin/kelolaPelanggan/delete/{id}', [kelolaPelangganController::class, 'delete'])->middleware(UserAkses::class . ':admin');
 
     Route::get('/logout', [SesiController::class, 'logout']);
+
+    Route::get('/profile', [ProfileController::class, 'index'])->name('profile.index');
+    Route::post('/profile/update', [ProfileController::class, 'update'])->name('profile.update');
+    Route::post('/profile/photo', [ProfileController::class, 'photo'])->name('profile.photo');
 });
 
 
